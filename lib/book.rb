3a816061ac50b42d31ae869440bce7d0309e8d4f
Anuj Panchal <anujpan1@gmail.com>
2018-10-10 20:07:49 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -2,8 +2,14 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
+  GENRES = []
+
   def initialize(title)
     @title = title
+  end
+  
+  def genre=(type)
+    GENRES.push(type)
   end
 
   def turn_page
